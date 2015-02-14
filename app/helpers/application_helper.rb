a3176a43dd88b37959cd7a835824e26521c0e7f1
Prasann Shah <preachx@gmail.com>
2015-02-14 17:16:09 +0530

    added css for diff media sizes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def error_messages_for(model)
+    return '' if model.errors.empty?
+
+    messages = model.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: model.errors.count,
+                      resource: model.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <h2>#{sentence}</h2>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
